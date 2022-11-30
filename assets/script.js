8747ffddfc167133c7d448e7b730004006651ff4
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2022-11-30 14:26:34 -0600

    added JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,34 @@
-
-
+function listTopics(){
+    for(var x = 0; x < listTopics.length; x++){
+        console.log(topics[x]);
+    }
+}
+listTopics()
+/*
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for(var x = 0; x < topics.length; x++){    
     console.log(topics[x]);
 }
-/*1
-var topic = "HTML";
 
-if (topic === 'HTML'){
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS'){
-    console.log("Let's study CSS!");
-} else if (topic === 'Git'){
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript'){
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
+var topic = "HTML";
+*/
+
+function selectTopic(){
+    if (topic === 'HTML'){
+        console.log("Let's study HTML!");
+    } else if (topic === 'CSS'){
+        console.log("Let's study CSS!");
+    } else if (topic === 'Git'){
+        console.log("Let's study Git!");
+    } else if (topic === 'JavaScript'){
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
 
- var shapes = ["triangle", "square", "pentagon", "circle"];
+/*
+var shapes = ["triangle", "square", "pentagon", "circle"];
 
 for(var x = 0; x < shapes.length; x++){
     //code block
